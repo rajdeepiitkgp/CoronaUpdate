9d55378e6e9fee3d635e9d83ace316caf46369fb
rajdeepiitkgp <rajdeep.biswas2010@gmail.com>
2020-04-03 13:20:31 +0530

    added left side bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { SideBarComponent } from './side-bar/side-bar.component';
+import { NgxEchartsModule } from 'ngx-echarts';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { SideBarComponent } from './side-bar/side-bar.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    NgxEchartsModule,
     SharedModule
   ],
   providers: [],
