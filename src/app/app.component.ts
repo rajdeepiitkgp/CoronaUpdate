14eedaa2f866e0f0aa3a2c54df312713a61aa90a
rajdeepiitkgp <rajdeep.biswas2010@gmail.com>
2020-04-06 14:48:00 +0530

    Fixed all mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,36 @@
-import { Component, AfterContentChecked, ChangeDetectorRef } from '@angular/core';
+import { Component, AfterContentChecked, ChangeDetectorRef, OnInit, OnDestroy } from '@angular/core';
 import { EventAggrigatorService } from './service/event-aggrigator.service';
+import { Router, NavigationEnd, Event } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterContentChecked {
+export class AppComponent implements AfterContentChecked, OnInit, OnDestroy {
   public progressFlag = false;
+  public opened = false;
+  private watcher: Subscription;
   constructor(
     private eventAggSrvc: EventAggrigatorService,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
+    private router: Router
   ) {
     this.eventAggSrvc.spinnerFlagChanged$.subscribe(value => {
       this.progressFlag = value;
+    });
+  }
+  ngOnDestroy(): void {
+    this.watcher.unsubscribe();
+  }
+  ngOnInit(): void {
+    this.watcher = this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        if (this.opened) {
+          this.opened = !this.opened;
+        }
+      }
     });
   }
   ngAfterContentChecked(): void {
@@ -21,6 +38,6 @@ export class AppComponent implements AfterContentChecked {
   }
 
   public openCloseSideBar(e) {
-    console.log(e);
+    this.opened = e.state;
   }
 }
--- src/app/top-navbar/top-navbar.component.ts
+++ src/app/top-navbar/top-navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, OnChanges, SimpleChange } from '@angular/core';
 
 
 @Component({
@@ -6,20 +6,26 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   templateUrl: './top-navbar.component.html',
   styleUrls: ['./top-navbar.component.scss']
 })
-export class TopNavbarComponent implements OnInit {
+export class TopNavbarComponent implements OnInit, OnChanges {
   public title = 'Dashboard';
   private sideBarClosed: boolean;
   @Output() emitOpenCloseSideBar: EventEmitter<any> = new EventEmitter();
+  @Input() parentState: boolean;
   public iconTrigger = 'menu';
   constructor() { }
 
   ngOnInit(): void {
     this.sideBarClosed = true;
   }
-
+  ngOnChanges(changes: { [propKey: string]: SimpleChange }): void {
+    if (!changes.parentState.currentValue) {
+      this.sideBarClosed = true;
+      this.setIcon();
+    }
+  }
   public openSidebar() {
     this.emitOpenCloseSideBar.emit({ state: this.sideBarClosed });
-    this.sideBarClosed = !this.sideBarClosed;
+    this.sideBarClosed = false;
     this.setIcon();
   }
 
